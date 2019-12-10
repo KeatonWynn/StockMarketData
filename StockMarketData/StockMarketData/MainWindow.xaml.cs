7053ab97876deed681fe8c6f46920aa363f55302
testuser <saxup@dim-coin.com>
2019-12-10 14:06:48 -0600

    GUI and functionality enhancments

--- StockMarketData/StockMarketData/MainWindow.xaml.cs
+++ StockMarketData/StockMarketData/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Win32;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
 
 namespace StockMarketData
 {
@@ -26,8 +28,14 @@ namespace StockMarketData
             InitializeComponent();
         }
 
-        string SelectFolderText;
-        
+        // variables
+        private static readonly string userHMF = Environment.UserName;
+        private static readonly string download_dir = "C:/Users/" + userHMF + "/Downloads";
+        private static readonly string chromedriver_dir = "C:/Users/" + userHMF + "/ImportFiles/";
+        private static readonly string date_format = "MM/dd/yyyy h:mm tt";
+        private string selected_folder;
+
+
         //empties textbox upon click
         public void Textbox_FileName_GotFocus(object sender, RoutedEventArgs e)
         {
@@ -36,22 +44,44 @@ namespace StockMarketData
             tb.GotFocus -= Textbox_FileName_GotFocus;
         }
 
-        //Select Directory button click action 
+        //Download Directory button click action 
         public void SelectDirectory_Button_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog o = new OpenFileDialog();
-
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.InitialDirectory = download_dir;
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                //if folder selected, will add more functionality later
+                selected_folder = dialog.FileName;
+            }
+            SelectDirectory_Button.Content = selected_folder;
         }
         
+        //Destination Directory button click action
+        public void SelectDestination_Button_Click(object sender, RoutedEventArgs e)
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.InitialDirectory = download_dir;
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                //if folder selected, will add more functionality later
+                selected_folder = dialog.FileName;
+            }
+            SelectDestination_Button.Content = selected_folder + "\\" + Textbox_FileName.Text;
+        }
 
         private void StartDownload_Button_Click(object sender, RoutedEventArgs e)
         {
-            SeleniumEnhanced.SeleniumEnhanced SE = new SeleniumEnhanced.SeleniumEnhanced();
-            SE.StartWebDriver();
 
             string StockText = Stock_TextBox.Text.ToUpper();
             string Selected_Website = ComboBoxWebsite.Text;
-            string yahoo_url = $"https://finance.yahoo.com/quote/{StockText}/history?p={StockText}" ;
+            string yahoo_url = $"https://finance.yahoo.com/quote/{StockText}/history?p={StockText}";
+            Log_TextBox.AppendText($"\n" + DateTime.Now.ToString(date_format) + ": Fetching data for " + StockText);
+
+            SeleniumEnhanced.SeleniumEnhanced SE = new SeleniumEnhanced.SeleniumEnhanced();
+            SE.StartWebDriver();            
 
 
             switch (Selected_Website)
@@ -60,6 +90,7 @@ namespace StockMarketData
                     Console.WriteLine($"Starting {StockText} Download");
                     SE.driver.Navigate().GoToUrl(yahoo_url);
                     Console.WriteLine($"Downloading File");
+                    System.Threading.Thread.Sleep(5000);
                     SE.GetFunction("Click", "//*[@id=\"Col1-1-HistoricalDataTable-Proxy\"]/section/div[1]/div[2]/span[2]/a/span", "Download");
                     SE.Quit();
                     break;
@@ -69,8 +100,8 @@ namespace StockMarketData
             
 
             }
-                
-                
+
+            Log_TextBox.AppendText($"\n" + DateTime.Now.ToString(date_format) + ": Data download complete");
 
 
         }
